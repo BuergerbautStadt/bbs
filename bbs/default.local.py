a501bef8d13849b42b239cb45eab69c56647aab0
Jochen Klar <jochenklar@gmail.com>
2015-03-23 21:48:01 +0100

    social buttons moved to core/layout_social.html, map options moved to options.js

--- bbs/default.local.py
+++ bbs/default.local.py
@@ -47,43 +47,5 @@ EMAIL_FROM = 'news@buergerbautstadt.de'
 # EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
 
-# FACEBOOK_APIKEY = '310881089105382'
+# FACEBOOK_APIKEY = 'XXXXXXXXXXXXXXX'
 
-TILES_URL = 'http://tiles.codefor.de/static/bbs/berlin/'
-TILES_OPT = {
-    'errorTileUrl': 'http://tiles.codefor.de/static/bbs/error.png',
-    'attribution': 'Map data &copy; 2012 OpenStreetMap contributors',
-    'maxZoom': 17,
-    'minZoom': 10,
-    'zIndex': 0,
-    'reuseTiles': True
-}
-TILES_URL_LOCAL = 'http://tiles.codefor.de/static/bbs/berlin/'
-TILES_OPT_LOCAL = {
-    'errorTileUrl': 'http://tiles.codefor.de/static/bbs/error.png',
-    'attribution': 'Map data &copy; 2012 OpenStreetMap contributors',
-    'attribution_local': 'Map data &copy; 2012 OpenStreetMap contributors, Geoportal Berlin',
-    'maxZoom': 17,
-    'minZoom': 9,
-    'zIndex': 0,
-    'reuseTiles': True
-}
-
-DEFAULT_VIEW = {
-    'lat': 52.51,
-    'lon': 13.37628,
-    'zoom': 11
-}
-
-NAVIGATION = [
-    {'text': 'Begriffe', 'href': '/begriffe'},
-    {'text': 'Liste', 'href': '/orte'},
-    {'text': 'Abo', 'href': '/news/abonnieren'}
-]
-
-INFO_TEXT = '''
-    <h2>Worum geht es hier?</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est. Lorem ipsum dolor sit amet.
-    </p>
-'''
