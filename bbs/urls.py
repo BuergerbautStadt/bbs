38c60c143ff318c6e8c36aba5348e101cead38c6
Jochen Klar <jochenklar@gmail.com>
2015-08-10 20:57:29 +0200

    urls updated

--- bbs/urls.py
+++ bbs/urls.py
@@ -12,7 +12,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='core/map.html')),
     url(r'^begriffe/$', 'wbc.process.views.process', name='process'),
-    url(r'^liste/$', TemplateView.as_view(template_name='process/list.html'), name='places'),
+    url(r'^liste/$', 'wbc.process.views.places', name='places'),
 
     # orte
     url(r'^orte/$', RedirectView.as_view(url='/liste/', permanent=True)),
